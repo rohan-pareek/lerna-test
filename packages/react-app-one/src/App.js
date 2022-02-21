ac117cd443f42105a42e121692f8015f4135ac59
Rohan Pareek <rohaniscoding@gmail.com>
2022-02-21 20:54:16 +0530

    Initial commit

---
+++ packages/react-app-one/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Lerna App</h1>
+     <p>App One</p>
+    </div>
+  );
+}
+
+export default App;
